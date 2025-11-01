a72bf2fa539f799a7ec07e771da5bd608c94d255
quantum-encoding <rich@quantumencoding.io>
2025-11-01 22:48:45 +0100

    [CHRONOS] 2025-11-01T21:48:44.+749346078Z::claude-code::::TICK-0000018206::[/home/founder]::[/home/founder/github_public/cognitive-telemetry-kit] → tool-completion - Read file: array_list.zig

---
+++ cognitive-state-server/.zig-cache/o/17864a756e5724567123c60dd5de3048/cimport.h.d
@@ -0,0 +1,32 @@
+.zig-cache/tmp/b54c45c17289faf3/cimport.zig: /usr/include/sqlite3.h \
+  /usr/local/zig/lib/compiler/aro/include/stdarg.h \
+  /usr/include/sys/inotify.h \
+  /usr/local/zig/lib/compiler/aro/include/stdint.h \
+  /usr/local/zig/lib/include/stdint.h /usr/include/stdint.h \
+  /usr/include/bits/libc-header-start.h /usr/include/features.h \
+  /usr/include/features-time64.h /usr/include/bits/wordsize.h \
+  /usr/include/bits/timesize.h /usr/include/stdc-predef.h \
+  /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+  /usr/include/gnu/stubs.h /usr/include/bits/types.h \
+  /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+  /usr/include/bits/wchar.h /usr/include/bits/stdint-intn.h \
+  /usr/include/bits/stdint-uintn.h /usr/include/bits/stdint-least.h \
+  /usr/include/bits/inotify.h /usr/include/dbus-1.0/dbus/dbus.h \
+  /usr/lib/dbus-1.0/include/dbus/dbus-arch-deps.h \
+  /usr/include/dbus-1.0/dbus/dbus-macros.h \
+  /usr/include/dbus-1.0/dbus/dbus-address.h \
+  /usr/include/dbus-1.0/dbus/dbus-types.h \
+  /usr/local/zig/lib/compiler/aro/include/stddef.h \
+  /usr/include/dbus-1.0/dbus/dbus-errors.h \
+  /usr/include/dbus-1.0/dbus/dbus-protocol.h \
+  /usr/include/dbus-1.0/dbus/dbus-bus.h \
+  /usr/include/dbus-1.0/dbus/dbus-connection.h \
+  /usr/include/dbus-1.0/dbus/dbus-memory.h \
+  /usr/include/dbus-1.0/dbus/dbus-message.h \
+  /usr/include/dbus-1.0/dbus/dbus-shared.h \
+  /usr/include/dbus-1.0/dbus/dbus-misc.h \
+  /usr/include/dbus-1.0/dbus/dbus-pending-call.h \
+  /usr/include/dbus-1.0/dbus/dbus-server.h \
+  /usr/include/dbus-1.0/dbus/dbus-signature.h \
+  /usr/include/dbus-1.0/dbus/dbus-syntax.h \
+  /usr/include/dbus-1.0/dbus/dbus-threads.h
